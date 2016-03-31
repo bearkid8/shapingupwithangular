90750b7c9526c5a8733a4ada63393b85d1d9153d
Mike K <johndoe@example.com>
2016-03-31 07:14:42 +0300

    angular tabs as separate module

--- js/app.js
+++ js/app.js
@@ -19,4 +19,14 @@
             canPurchase: true
         }
     ];
+
+    app.controller('PanelController', function () {
+        this.tab = 1;
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        }
+        this.isSelected = function (checkedTab) {
+            return this.tab === checkedTab;
+        }
+    });
 })();
