8838fb7794c4bfc4fede5c4b064033e5e2ffc423
Mike K <johndoe@example.com>
2016-03-31 05:28:38 +0300

    first angular app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-var app = angular.module('store', [ ]);
+(function () {
+    var app = angular.module('store', [ ]);
+
+    app.controller('StoreConntroller', function () {
+        this.product = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: '. . .',
+            canPurchase: true,
+            soldOut: true
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 2.95,
+            description: '. . .',
+            canPurchase: true,
+            soldOut: true
+        }
+    ];
+})();
