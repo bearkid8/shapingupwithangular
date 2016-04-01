20eeec8e981005f90d20c42778c477ac532484ac
Mike K <johndoe@example.com>
2016-04-01 08:35:29 +0300

    forms live editing

--- js/app.js
+++ js/app.js
@@ -8,25 +8,74 @@
     var gems = [
         {
             name: 'Dodecahedron',
+            price: 250.00,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                },
+                {
+                    stars: 4,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                }
+            ]
+        },
+        {
+            name: 'Dodecahedron',
             price: 2.95,
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            canPurchase: true
+            canPurchase: true,
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                },
+                {
+                    stars: 4,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                }
+            ]
         },
         {
             name: 'Pentagonal Gem',
             price: 2.95,
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            canPurchase: true
+            canPurchase: true,
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                },
+                {
+                    stars: 4,
+                    body: "I love this product!",
+                    author: "joe@thomas.com"
+                }
+            ]
         }
     ];
 
     app.controller('PanelController', function () {
-        this.tab = 1;
+        this.tab = 3;
         this.selectTab = function (setTab) {
             this.tab = setTab;
         }
         this.isSelected = function (checkedTab) {
             return this.tab === checkedTab;
+        }
+    });
+
+    app.controller("ReviewController", function () {
+        this.review = {};
+
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
         }
     });
 })();
