4bef03a6314f8db8d6183a00604ed15ab2a56d42
Mike K <johndoe@example.com>
2016-04-02 06:45:57 +0300

    servisec, receiving data from json file

--- js/app.js
+++ js/app.js
@@ -1,65 +1,73 @@
 (function () {
     var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function () {
-        this.products = gems;
-    });
+    app.controller('StoreController', ['$http',function ($http) {
+        // this.products = gems;
+        var store = this;
+        store.products = [];
 
-    var gems = [
-        {
-            name: 'Dodecahedron',
-            price: 250.00,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            reviews: [
-                {
-                    stars: 5,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                },
-                {
-                    stars: 4,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                }
-            ]
-        },
-        {
-            name: 'Dodecahedron',
-            price: 2.95,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            canPurchase: true,
-            reviews: [
-                {
-                    stars: 5,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                },
-                {
-                    stars: 4,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                }
-            ]
-        },
-        {
-            name: 'Pentagonal Gem',
-            price: 2.95,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            canPurchase: true,
-            reviews: [
-                {
-                    stars: 5,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                },
-                {
-                    stars: 4,
-                    body: "I love this product!",
-                    author: "joe@thomas.com"
-                }
-            ]
-        }
-    ];
+        $http.get('data/products.json').success(function (data) {
+            store.products = data;
+        });
+    }]);
+
+
+
+    // var gems = [
+    //     {
+    //         name: 'Dodecahedron',
+    //         price: 250.00,
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         reviews: [
+    //             {
+    //                 stars: 5,
+    //                 body: "I love this product!",
+    //                 author: "joe@thomas.com"
+    //             },
+    //             {
+    //                 stars: 4,
+    //                 body: "I love this product!",
+    //                 author: "joe@thomas.com"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         name: 'Dodecahedron',
+    //         price: 2.95,
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         canPurchase: true,
+    //         reviews: [
+    //             {
+    //                 stars: 5,
+    //                 body: "I love this product!",
+    //                 author: "joe@thomas.com"
+    //             },
+    //             {
+    //                 stars: 4,
+    //                 body: "I love this product!",
+    //                 author: "joe@thomas.com"
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         name: 'Pentagonal Gem',
+    //         price: 2.95,
+    //         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         canPurchase: true,
+    //         reviews: [
+    //             {
+    //                 stars: 5,
+    //                 body: "I love this product!",
+    //                 author: "joe@thomas.com"
+    //             },
+    //             {
+    //                 stars: 4,
+    //                 body: "I love this product!",
+    //                 author: "joe@thomas.com"
+    //             }
+    //         ]
+    //     }
+    // ];
 
     // app.controller('PanelController', function () {
     //     this.tab = 3;
